5550df33ce3121524db9bdf9b0ce01e4be3b975d
Khalid Hasan <sawmaster22@gmail.com>
2023-12-06 11:02:10 +0100

    add: driver object and scripts

---
+++ Assets/Scripts/Driver.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+    float steerSpeed = 0.1f;
+    float moveSpeed = 0.002f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, steerSpeed);
+        transform.Translate(0, moveSpeed, 0);
+    }
+}
